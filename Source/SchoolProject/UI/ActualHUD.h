02c1a288bd19bc99259c0517d2a20baac4ddde15
DanielClone <25902541@stu.mmu.ac.uk>
2026-05-08 16:07:13 +0100

    Main pushh

---
+++ Source/SchoolProject/UI/ActualHUD.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ActualHUD.generated.h"
+
+
+
+class UTextBlock;
+
+class UProgressBar;
+
+
+
+UCLASS()
+
+class SCHOOLPROJECT_API UPlayerHud : public UUserWidget
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, meta = (Bindwidget))
+	TObjectPtr<UTextBlock> HealthText;
+
+	UPROPERTY(EditAnywhere, meta = (Bindwidget))
+	TObjectPtr<UProgressBar> HealthBar;
+
+public:
+	UFUNCTION()
+	virtiual void NativeConstruct() override;
+
+	UFUNCTION()
+	void SetHealthBar(float CurrentHealth, float MaxHealth);
+};
---
+++ Source/SchoolProject/UI/PlayerHud2.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PlayerHud.generated.h"
+
+class UProgressBar;
+
+class UTextBlock;
+
+
+UCLASS()
+class SCHOOLPROJECT_API UPlayerHud : public UUserWidget
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, meta = (Bindwidget))
+	TObjectPtr<UTextBlock> HealthText;
+
+	UPROPERTY(EditAnywhere, meta = (Bindwidget))
+	TObjectPtr<UProgressBar> HealthBar;
+
+
+public:
+	UFUNCTION()
+	virtiual void NativeConstruct() override;
+
+	UFUNCTION()
+	void SetHealthBar(float CurrentHealth, float MaxHealth);
+};
